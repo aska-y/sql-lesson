16bcc0c14efb7efce7709540b051dcb2976d0901
aska-y <85142247+aska-y@users.noreply.github.com>
2022-10-01 11:37:59 +0900

    updated app.py

--- app.py
+++ app.py
@@ -98,8 +98,11 @@ def delete(id):
 
 
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port=5001, threaded=True)
+# if __name__ == '__main__':
+#     app.run(host = '0.0.0.0', port=5001, threaded=True)
 
-
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
+    app.run()
 
